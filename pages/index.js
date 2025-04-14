6dbfd57c0818a0e7d1082687364acd7dcf35c1c0
SISITheAI <sisibernoulli@gmail.com>
2025-04-14 15:32:51 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() { return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>Bienvenue sur SISI DANIEL IA</h1>
+      <p>Une intelligence artificielle pour poser des questions, générer des images et plus encore !</p>
+    </div>
+  );
+}
